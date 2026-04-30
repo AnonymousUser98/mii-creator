86f084f81e1d2c7d461a483175adde3f0dd92e69
AnonymousUser98 <161506704+AnonymousUser98@users.noreply.github.com>
2026-04-30 14:54:08 -0700

    Fix file path in import command

--- src/ui/setup.ts
+++ src/ui/setup.ts
@@ -22,7 +22,7 @@ import { Config } from "../config";
 import Html from "@datkat21/html";
 import { AddButtonSounds } from "../util/AddButtonSounds";
 import { customRender } from "./pages/library/render/customRender";
-import { importMiiConfirmation } from "./library/importDialog";
+import { importMiiConfirmation } from "./pages/library/importDialog";
 
 export async function setupUi() {
   let mm = getMusicManager();
