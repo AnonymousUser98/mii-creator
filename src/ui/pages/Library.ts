66c5682bbf7ec1d7b95af63a432b10e1dd2f37e0
AnonymousUser98 <161506704+AnonymousUser98@users.noreply.github.com>
2026-05-01 12:35:31 -0700

    Move import button to top of sidebar

--- src/ui/pages/Library.ts
+++ src/ui/pages/Library.ts
@@ -369,6 +369,18 @@ export async function Library(highlightMiiId?: string) {
         })
       ),
       AddButtonSounds(
+        new Html("button").text("Import from Base64").on("click", () => {
+          var miiDataInput = prompt("Paste the Base64 data here:");
+          var mii = new Mii(
+            Buffer.from(
+              miiDataInput,
+              "base64"
+            )
+          );
+          importMiiConfirmation(mii, "Imported from Base64");
+        })
+      ),
+      AddButtonSounds(
         new Html("button").text("Settings").on("click", async () => {
           Settings();
         })
@@ -642,17 +654,7 @@ export async function Library(highlightMiiId?: string) {
           alert("Invalid password.");
       }
     }
-  ),
-  new Html("button").text("Import from Base64").on("click", () => {
-    var miiDataInput = prompt("Paste the Base64 data here:");
-    var mii = new Mii(
-      Buffer.from(
-        miiDataInput,
-        "base64"
-      )
-    );
-    importMiiConfirmation(mii, "Imported from Base64");
-  })
+  )
 );
 }
 
