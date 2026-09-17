36652c16a90502f209e8ffcaa31daf3b26f35687
AnonymousUser98 <161506704+AnonymousUser98@users.noreply.github.com>
2026-09-17 09:28:52 -0700

    Re-enable Wii U theme

--- src/ui/pages/Settings.ts
+++ src/ui/pages/Settings.ts
@@ -60,9 +60,9 @@ export const updateSettings = async (force: boolean = false) => {
   const theme = await localforage.getItem("settings_theme");
   if (theme === null) {
     await setSetting("theme", "default");
-  } else if (theme === "wiiu") {
+  } /* else if (theme === "wiiu") {
     await setSetting("theme", "default");
-  }
+  } */
 
   // Theme selector
   document.documentElement.dataset.theme = String(
