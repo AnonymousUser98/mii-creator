41693060efc18495d14b28f52bed982030e82861
AnonymousUser98 <161506704+AnonymousUser98@users.noreply.github.com>
2026-09-18 10:35:57 -0700

    Add 3DS shader to BodyShaderTypes.ts

--- src/constants/BodyShaderTypes.ts
+++ src/constants/BodyShaderTypes.ts
@@ -12,6 +12,7 @@ export enum BodyType {
 export enum ShaderType {
   WiiU = "wiiu",
   Switch = "switch",
+  3DS = "3ds",
   LightDisabled = "lightDisabled",
   Miitomo = "miitomo",
   MiitomoBasic = "miitomo_basic",
@@ -27,6 +28,7 @@ export function adjustShaderQuery(params: URLSearchParams, shader: ShaderType) {
     case ShaderType.WiiU:
     case ShaderType.Switch:
     case ShaderType.Miitomo:
+    case ShaderType.3DS:
       // share the same type string so can be used directly
       params.set("shaderType", shader);
       break;
