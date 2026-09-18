a6ff661116e331a6627ed3f4b10cbd1acee1b306
AnonymousUser98 <161506704+AnonymousUser98@users.noreply.github.com>
2026-09-18 11:16:51 -0700

    Change 3DS to ThreeDS

--- src/constants/BodyShaderTypes.ts
+++ src/constants/BodyShaderTypes.ts
@@ -12,7 +12,7 @@ export enum BodyType {
 export enum ShaderType {
   WiiU = "wiiu",
   Switch = "switch",
-  3DS = "3ds",
+  ThreeDS = "3ds",
   LightDisabled = "lightDisabled",
   Miitomo = "miitomo",
   MiitomoBasic = "miitomo_basic",
@@ -28,7 +28,7 @@ export function adjustShaderQuery(params: URLSearchParams, shader: ShaderType) {
     case ShaderType.WiiU:
     case ShaderType.Switch:
     case ShaderType.Miitomo:
-    case ShaderType.3DS:
+    case ShaderType.ThreeDS:
       // share the same type string so can be used directly
       params.set("shaderType", shader);
       break;
